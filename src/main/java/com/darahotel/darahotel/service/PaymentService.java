15d93dd6d7adc7bf49ec55b0103839a54a65a9f2
LonDara <daralon2020@gmail.com>
2024-06-23 19:47:56 +0700

    modify a little bit

--- src/main/java/com/darahotel/darahotel/service/PaymentService.java
+++ src/main/java/com/darahotel/darahotel/service/PaymentService.java
@@ -5,7 +5,6 @@ import com.darahotel.darahotel.entity.PaymentSlip;
 
 
 import java.util.List;
-import java.util.Optional;
 
 public interface PaymentService {
     CheckoutResponse createPayment(Long bookingId);
